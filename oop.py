cb8fbbd57baf20364102f6cfd366590e093492d2
Anniymm <animaisuradze1999@gmail.com>
2026-03-27 20:55:34 +0400

    add classes

--- oop.py
+++ oop.py
@@ -27,6 +27,41 @@
 # class (atributebi) --konstruqtori-> object
 
 
-# ---------------------------------------------------------------------
+# # ---------------------------------------------------------------------
 
 
+# class Dog:
+#     def __init__(self, name, feature):
+#         self.name = name # atributebi
+#         self.feature = feature 
+    
+#     def barks(self): # method 
+#         print(f"{self.name} is {self.feature}")
+
+
+# my_dog = Dog("Buddy", "bark") # object
+# my_dog.barks()
+
+# my_dog_2 = Dog("vivi", "eating")
+# my_dog_2.barks()
+# print(my_dog_2.feature)
+# # print(f"{my_dog.name} is {my_dog.feature}ing")
+
+# --------------------------------------------------------------------------
+
+class Car:
+    def set_details(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def show_details(self):
+        print(f"{self.brand}, color: {self.color}")
+
+
+car = Car()
+# print(car.color)
+car.set_details("wrx", "black")
+print(car.color)
+car.show_details()
+
+# ------------------------------------------------------------------------------
