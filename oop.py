ed4ff00a83e849cad5bb16dd2ed250dc3e98f129
Anniymm <animaisuradze1999@gmail.com>
2026-03-27 20:41:16 +0400

    add small oop script

---
+++ oop.py
@@ -0,0 +1,32 @@
+# OOP - class, objects, attribute
+# object-oriented programming
+#
+# class ClassName - charcho saidanac unda vawarmoot objectebi - 
+# modeli, dzravis moculoba, sawvavis tipi - classhi vwert - atributebi
+# wrx, 2.0, dizeli - object 1
+# 2_modeli, 3.5, sawvavi - object 2 
+
+# object - classis instance 
+
+# cvladi - atributi
+# method = function (def)
+
+# --------------------------------------------------------------------------
+
+# atributebi - name, feature 
+# __init__ - konstruqtori - methodi romelic gamoiyeneba inicializaciistvis 
+
+# class Dog:
+#     def __init__(self, name, feature):
+#         self.name = name # atributebi
+#         self.feature = feature 
+
+# my_dog = Dog("Buddy", "bark") # object
+# print(f"{my_dog.name} is {my_dog.feature}ing")
+
+# class (atributebi) --konstruqtori-> object
+
+
+# ---------------------------------------------------------------------
+
+
